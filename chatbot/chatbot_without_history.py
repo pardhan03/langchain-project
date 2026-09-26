878a665cd544a95af9ab71b0fbb1c6216c207434
Manish Pardhan <pardhanmanish7887@gmail.com>
2026-09-26 22:05:12 +0530

    langchain in memory

--- chatbot/chatbot_without_history.py
+++ chatbot/chatbot_without_history.py
@@ -1,6 +1,5 @@
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 
 llm = ChatOllama(
